6dcfe5a9a13093de63736cfdccbe21053a63e8bd
phani <phanidharguttikonda0@gmail.com>
2025-01-30 14:51:58 +0530

    Added configarations file

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secreatKey : "Phani@9090K"
+}
--- backend/routes/Authentication.js
+++ backend/routes/Authentication.js
@@ -1,34 +1,38 @@
-﻿const {Router} = require('express') ;
-const router = Router() ;
-const {inputValidation, emailValidation,userNameCheck} = require('../middleware/Authentication') ;
-const jwt = require('jsonwebtoken') ;
-const TodoApp = require('../db/Todos') ;
+﻿const { Router } = require('express');
+const router = Router();
+const { inputValidation, emailValidation, userNameCheck } = require('../middleware/Authentication');
+const jwt = require('jsonwebtoken');
+const TodoApp = require('../db/Todos');
+const {secreatKey} = require('../config') ;
 
-router.post('/sign-up', inputValidation, emailValidation,userNameCheck, async (req,res) => {
-const email = req.body.email; 
-const password = req.body.password ;
-const username = req.body.username ;
+router.post('/sign-up', inputValidation, emailValidation, userNameCheck, async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
 
-await TodoApp.create({
-email,
-username,
-password
+    await TodoApp.create({
+        email,
+        username,
+        password
+    });
+
+    return res.json({ message: "sucessfully Created User", value: true })
+
 });
 
-return res.json({message: "sucessfully Created User", value: true})
+router.post('/sign-in', inputValidation, async (req, res) => {
+    const password = req.body.password;
+    const username = req.body.username;
 
-}) ;
+    const result = await TodoApp.findOne({ username, password });
+    if (result) return res.json({ message: "user Exists", value: true });
+    // here we are going to return the jwt 
+    
+    return res.json({ message: "user doesn't exists", value: false, 
+        authorization:  jwt.sign({username, expiry: Date.now() + 3600},secreatKey) }); 
+        // means the session will be only for 
 
-router.post('/sign-in', inputValidation, async (req,res) => {
-const password = req.body.password ;
-const username = req.body.username ;
-
-const result = await TodoApp.findOne({username, password}) ;
-if(result) return res.json({message: "user Exists", value: true}) ;
-
-return res.json({message: "user doesn't exists",value: false}) ;
-
-}) ;
+});
 
 module.exports = router;
 
