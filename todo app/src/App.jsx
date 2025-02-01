387870b3e30e52559cb4fd3b6b9791124250ce64
phani <phanidharguttikonda0@gmail.com>
2025-02-01 20:57:20 +0530

    intialized react project

--- todo app/src/App.jsx
+++ todo app/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 
 
 function App() {
-
+  
 
   return (
-    <div></div>
+    <>
+      
+    </>
   )
 }
 
-
-// we have deleted the App.css and index.css files as we don't need them and also , we are going to use tailwind
-// and we also deleted assets folder and public as we don't need them
-export default App ;
+export default App
